e819f81bc87f301c9b44574fef6072efb2a59fe2
Alex Landherr <alex.landherr@gmail.com>
2023-10-06 13:30:01 +0000

    Added number grid as txt file.

--- src/include/functions.h
+++ src/include/functions.h
@@ -1,8 +1,12 @@
-
+#include <array>
+#include <string>
 
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-namespace func {}
+namespace func {
+    std::array<std::array<std::string, 20>, 20> txt_file_to_array(std::string file_path);
+    int find_greatest_grid_product();
+}
 
 #endif
